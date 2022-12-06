b6401e3910e64a7747bee0590f4877891dabc0bf
Luke Francis <lukebradonfrancis@gmail.com>
2022-12-05 19:05:21 -0500

    worked with variable data types

---
+++ variabledatatypes.py
@@ -0,0 +1,7 @@
+character_name = "Jake"
+character_age = "30"
+is_male = True 
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
